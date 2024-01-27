a53c1e75e8470c73a455009eea4c37641e829f7e
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-01-27 04:26:20 -0500

    pulling

--- server/app.js
+++ server/app.js
@@ -1,32 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { auth, requiresAuth } = require('express-openid-connect')
-
-// AUTH0 CONFIG 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: 'xDGHzMPBoth79F9I6Mc4DgMZp2j7pg25',
-  issuerBaseURL: 'https://dev-h8c1qgz2u0vwfqsz.us.auth0.com'
-}
-
 const app = express()
 app.use(cors())
-app.use(auth(config))
 const port = 5000
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-// AN EXAMPLE ROUTE THAT REQUIRES AUTH TO GO TO.
-// app.get('/profile', requiresAuth(), (req, res) => {
-//   res.send(JSON.stringify(req.oidc.user));
-// });
 
 
 app.listen(port, () => {
