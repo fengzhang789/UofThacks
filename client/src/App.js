156ab1ec41e49b93b2798ed1df7123c607e2895d
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-01-27 11:46:45 -0500

    upload

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Profile from './pages/Profile';
 import Home from './pages/Home'
 import Navbar from './components/Navbar';
+import Upload from './pages/Upload';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
+          <Route path='/upload' element={<Upload />}></Route>
       </Routes> 
     </>
   );
---
+++ client/src/pages/Upload.js
@@ -0,0 +1,23 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+import NotLoggedInPage from './NotLoggedInPage';
+
+// THIS IS THE PAGE WHERE THE USER TAKES AND UPLOADS THEIR PHOTO
+const Upload = () => {
+    const {isAuthenticated} = useAuth0();
+    
+    return (
+        <>
+            {isAuthenticated ? (
+                <div>
+                    
+                </div>
+            ): (
+                <NotLoggedInPage />
+            )}
+        </>
+        
+    )
+}
+
+export default Upload
