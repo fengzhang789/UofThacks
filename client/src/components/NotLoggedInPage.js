40660ce6b8a5793061bf64e0ccf4e20ed7f921f2
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-01-27 04:34:26 -0500

    added not logged in error page

---
+++ client/src/components/NotLoggedInPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotLoggedInPage = () => {
+    return (
+        <div className='flex flex-row w-screen h-screen text-center items-center'>
+            <h1>Sorry! You need to login to access this page :( </h1>
+        </div>
+    )
+}
+
+export default NotLoggedInPage
