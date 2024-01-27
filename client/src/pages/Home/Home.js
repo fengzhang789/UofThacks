90c604125baff14b19fb776d67ce3669b1089548
Ally Guo <69687800+ally-223@users.noreply.github.com>
2024-01-27 11:18:39 -0500

    css update

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,9 +5,9 @@ import '../Home/Home.css'
 const Home = () => {
     return (
         <div>
-            <button class>Login</button>
-            <h4>Don't have an account? Sign-up here</h4>
-            <img></img>
+            <button className='btn1'>Login to my account</button>
+            <h4 className='signup_h4'>Don't have an account? Sign-up here</h4>
+            
         </div>
     )
 }
