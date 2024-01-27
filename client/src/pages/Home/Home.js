69416659208d11b87cf0395d26ed15d5994917b5
Ally Guo <69687800+ally-223@users.noreply.github.com>
2024-01-27 11:46:20 -0500

    add bottles file

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import '../Home/Home.css'
 
 
 const Home = () => {
     return (
         <div>
+            <Link> 
             <button className='btn'>Throw a drift bottle!</button>
-            <a></a>
+            </Link>
+            
+            
             
         </div>
     )
