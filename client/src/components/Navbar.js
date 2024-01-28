c1b9f1299f0faedc59e66c1c28bca4848e488710
Irisimoo <irismo1009@gmail.com>
2024-01-28 01:58:46 -0500

    fix navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,16 +2,26 @@ import React from 'react'
 import Login from '../components/Login'
 import Logout from '../components/Logout'
 import { useAuth0 } from '@auth0/auth0-react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const {isAuthenticated, isLoading, user} = useAuth0()
 
     return (
-        <div className='navbar flex flex-row p-8'>
-            <header className="w-1/2 text-left">
-                ABOT O' WOT
-            </header>
-            <div  className="w-1/2 text-right">
+        <div className='navbar flex flex-row justify-between items-center p-6'>
+            <ul className='flex flex-row justify-between items-center'>
+            <li className="text-left flex-grow m-5">
+                <Link to='/'>ABOT O' WOT</Link>
+            </li>
+            <li className="text-left flex-grow m-5">
+                {isAuthenticated ? (
+                    <Link to='/profile'>Profile</Link>
+                ) : (
+                    <></>
+                )}
+            </li>
+            </ul>
+            <div  className="text-right flex-grow m-5">
                 <nav>
                     {isAuthenticated ? (
                         <Logout />
--- client/src/pages/NotLoggedInPage.js
+++ client/src/pages/NotLoggedInPage.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Home/Home.css'
 
 const NotLoggedInPage = () => {
     return (
-        <div className='flex flex-row w-screen h-40 text-center items-center'>
+        <div className='flex flex-row w-screen text-center items-center'>
             <h4 className='errormsg'>Login to throw a drift bottle!</h4> 
         </div>
     )
